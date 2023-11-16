c7ad504d574c3185df4c66976f2119d5e265a880
nir-nahum <45360833+nir-nahum@users.noreply.github.com>
2023-11-16 18:24:15 +0200

    Create rules.js
    Add rules

---
+++ src/OpenAI/rules.js
@@ -0,0 +1,18 @@
+let rules = [];
+export { getRules };
+
+rules.push({audience: "hitech", platform: "*", question: "Does the following text use phrases that relate to high technology workers?", grade: "good", response: "Well done, you used hitech langugae"});
+
+function getRules(audience, platform)
+{
+    var result = [];
+
+    for(rule in rules)
+    {
+        if(rule.audience == audience || rule.audience == "*")
+            if(rule.platform == platform || rule.platform == "*")
+                result.push(rule);
+    }
+
+    return result;
+}
