d6ea59452e13d477ed9a70bdd6a5cdad6a2e4dde
Nir Nahum <nir@walkme.com>
2023-11-16 17:24:52 +0200

    Adding OpenAI stuff (disabled)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./Context/theme";
+import "./OpenAI/openai.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
