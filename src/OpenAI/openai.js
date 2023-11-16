f39d7713277c52dbd68aa051828ce8cf7798e923
nir-nahum <45360833+nir-nahum@users.noreply.github.com>
2023-11-16 18:49:07 +0200

    Fix

--- src/OpenAI/openai.js
+++ src/OpenAI/openai.js
@@ -1,5 +1,6 @@
 import OpenAI from 'openai'
 import { open_ai_key } from './openai-key.js'
+import { getRules } from './rules.js'
 
 const openai = new OpenAI({
   apiKey: open_ai_key,
@@ -8,7 +9,14 @@ const openai = new OpenAI({
 
 export async function parseText(text) {
 
-  const questions = ['Is it talking about hospital?','Is it talking about the war?','Is it talking about peace?','Is it talking about revenge?','* Is it talking about the holocaust?'];
+  //const questions = ['Is it talking about hospital?','Is it talking about the war?','Is it talking about peace?','Is it talking about revenge?','* Is it talking about the holocaust?'];
+
+  var rules = getRules("hitech", "facebook");
+
+  var questions = [];
+  rules.forEach(function (rule, index) {
+    questions.push(rule.question);
+  });
 
   const completion = await openai.chat.completions.create({
     messages: [
@@ -17,7 +25,7 @@ export async function parseText(text) {
         content: `You are a helpful assistant designed to output JSON. 
         I will provide you with a post submitted by a user and will you will answer few questions about it. 
         You should output a JSON with an array that every item in the array corresponds to one my questions.
-        Each array item should include: The original question, answer(true/false) and the snippet of text from the article 
+        Each array item should include: The original question, answer true/false per the result of the question, and the snippet of text from the article 
         relevant to the answer
         The post: ${text}
         The questions: ${questions}"`,
--- src/OpenAI/rules.js
+++ src/OpenAI/rules.js
@@ -2,6 +2,7 @@ let rules = [];
 export { getRules };
 
 rules.push({audience: "hitech", platform: "*", question: "Does the following text use phrases that relate to high technology workers?", grade: "good", response: "Well done, you used hitech langugae"});
+rules.push({audience: "*", platform: "*", question: "Is it using bad language?", grade: "bad", response: "Don't use bad language"});
 
 function getRules(audience, platform)
 {
