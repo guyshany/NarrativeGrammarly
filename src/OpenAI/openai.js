2f7df38a4ad5ae6d6f4dc2f6f35687d8a21f39c0
nir-nahum <45360833+nir-nahum@users.noreply.github.com>
2023-11-16 18:00:00 +0200

    Update openai.js
    Fix

--- src/OpenAI/openai.js
+++ src/OpenAI/openai.js
@@ -5,12 +5,7 @@ const openai = new OpenAI({ apiKey: open_ai_key, dangerouslyAllowBrowser: true }
 
 export async function parseText(text) {
 
-
-  const questions = `* Is it talking about hospital?
-* Is it talking about the war?
-* Is it talking about peace?
-* Is it talking about revenge?
-* Is it talking about the holocaust?`;
+  const questions = ['Is it talking about hospital?','Is it talking about the war?','Is it talking about peace?','Is it talking about revenge?','* Is it talking about the holocaust?'];
 
   const completion = await openai.chat.completions.create({
     messages: [
@@ -18,15 +13,17 @@ export async function parseText(text) {
         role: "system",
         content: `You are a helpful assistant designed to output JSON. 
         I will provide you with a post submitted by a user and will you will answer few questions about it. 
-        The answers should be in JSON and include: The original question, answer(true/false) and the snippet of text from the article 
+        You should output a JSON with an array that every item in the array corresponds to one my questions.
+        Each array item should include: The original question, answer(true/false) and the snippet of text from the article 
         relevant to the answer
-        The article: ${text}
+        The post: ${text}
         The questions: ${questions}"`
       },
     ],
     model: "gpt-3.5-turbo-1106",
     response_format: { type: "json_object" },
   });
+
   console.log(completion.choices[0].message.content);
 
   return completion.choices[0].message.content;
